a89ee5e2ecbda66312309535d41b49df18f1f23d
paulsikabalu <sikabalu@outlook.com>
2024-01-01 09:30:30 +0200

    AirBnb v2

--- web_flask/10-hbnb_filters.py
+++ web_flask/10-hbnb_filters.py
@@ -1,31 +1,27 @@
 #!/usr/bin/python3
-"""Starts a Flask web application.
+# -*- coding: utf-8 -*-
 
-The application listens on 0.0.0.0, port 5000.
-Routes:
-    /hbnb_filters: HBnB HTML filters page.
-"""
 from models import storage
-from flask import Flask
-from flask import render_template
-
+from models.state import State
+from models.amenity import Amenity
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route("/hbnb_filters", strict_slashes=False)
-def hbnb_filters():
-    """Displays the main HBnB filters HTML page."""
-    states = storage.all("State")
-    amenities = storage.all("Amenity")
-    return render_template("10-hbnb_filters.html",
-                           states=states, amenities=amenities)
-
-
 @app.teardown_appcontext
-def teardown(exc):
-    """Remove the current SQLAlchemy session."""
+def appcontext_teardown(self):
+    """use storage for fetching data from the storage engine
+    """
     storage.close()
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+@app.route('/hbnb_filters', strict_slashes=False)
+def state_id():
+    """Display a HTML page inside the tag BODY"""
+    return render_template('10-hbnb_filters.html',
+                           states=storage.all(State),
+                           amenities=storage.all(Amenity))
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,35 +1,27 @@
 #!/usr/bin/python3
-"""Starts a Flask web application.
+# -*- coding: utf-8 -*-
 
-The application listens on 0.0.0.0, port 5000.
-Routes:
-    /: Displays 'Hello HBNB!'.
-    /hbnb: Displays 'HBNB'.
-    /c/<text>: Displays 'C' followed by the value of <text>.
-"""
 from flask import Flask
-
 app = Flask(__name__)
 
 
-@app.route("/", strict_slashes=False)
-def hello_hbnb():
-    """Displays 'Hello HBNB!'."""
-    return "Hello HBNB!"
+@app.route('/', strict_slashes=False)
+def hello():
+    """Start a basic Flask web application"""
+    return 'Hello HBNB!'
 
 
-@app.route("/hbnb", strict_slashes=False)
+@app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """Displays 'HBNB'."""
-    return "HBNB"
+    """Adding a specific route /hbnb"""
+    return 'HBNB'
 
 
-@app.route("/c/<text>", strict_slashes=False)
-def c(text):
-    """Displays 'C' followed by the value of <text>."""
-    text = text.replace("_", " ")
-    return "C {}".format(text)
+@app.route('/c/<string:text>', strict_slashes=False)
+def text(text=None):
+    """Dynamic inputed text: replace _ for space and show text"""
+    return "C {}".format(text.replace('_', ' '))
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
